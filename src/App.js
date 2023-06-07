5a4195611df68a40ca8698583cd22015dbf85318
Vuong Q. Huy <119883477+vnghuygithub@users.noreply.github.com>
2023-06-07 09:47:08 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const gifts = [
+  'Keyboard',
+  'Magic Mouse',
+  'Apple Watch'
+]
 
 function App() {
+
+  const [gift, setGift] = useState()
+
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+    setGift(gifts[index])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      <h1 >{gift || "Do u want a gift?"}</h1>
+      <button onClick={randomGift}>Claim</button>
     </div>
   );
 }
